e42fe3a9622934dd6e04969a077e4527e4450776
def depresion{} <depresion@pop-os.localdomain>
2020-10-01 16:20:04 -0500

    Agregamos los archivos

---
+++ ejercicio5.py
@@ -0,0 +1,7 @@
+            #Programa para sacar el valor del perimetro de un cuadrado
+
+lado = int(input("Ingrese el lado del cuadrado: \n"))
+
+perimetro_cuadrado = lado * 4
+
+print "El perimetro del cuadrado es: \n  ",perimetro_cuadrado

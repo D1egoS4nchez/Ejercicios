3b58abc15a9d039bcd006220517220e346df3678
def depresion{} <depresion@pop-os.localdomain>
2020-10-01 17:14:54 -0500

    Archivos faltantes

---
+++ Ejercicios/ejercicio2.py
@@ -0,0 +1,6 @@
+lado=input("Ingrese la medida del lado del cuadrado:") # Con la funcion input se permite ingresar datos, esta funcion retorna TODOS los caracteres escritos por el operador del programa
+lado=int(lado)
+superficie= lado*lado
+"""print("La superficie del cuadrado es:",superficie)"""
+print("La superficie del cuadrado es:")
+print(superficie) #Esto es un comentario
---
+++ Ejercicios/ejercicio4.py
@@ -0,0 +1,7 @@
+precio = int(input("Ingrese el precio del prodcuto: "))
+
+cantidad = int(input("Ingrese la cantidad que va a llevar: "))
+
+importe = precio * cantidad
+
+print"La cantidad total a pagar es: \n", importe

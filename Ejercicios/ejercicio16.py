d5afa41b3c41e987f93a453e0260e867d82b9f7b
Itachi_Uchiha <itachi_uchiha@pop-os.localdomain>
2020-10-02 18:17:27 -0500

    Modificados

--- Ejercicios/ejercicio16.py
+++ Ejercicios/ejercicio16.py
@@ -1,1 +1,12 @@
-#[200~Se ingresa por teclado un valor entero, mostrar una leyenda que indique si el número es positivo, negativo o nulo (es decir cero)]]
+numero = int(input("Ingrese el numero: "))
+
+if numero == 0: 	#Se comprueba que el numero sea 0
+	print("El numero es nulo")
+
+else: #Si este no es cero, se pasa a la siguiente funcion
+
+	if numero >= 0: #Se ejecuta la condicion base
+		print("El numero es positivo")
+
+	else: #Si la condicion de arriba no se cumple, este sacará que es negativo
+		print("El numero es negativo")
--- Ejercicios/ejercicio17.py
+++ Ejercicios/ejercicio17.py
@@ -1,12 +1,12 @@
 num1 = int(input("Ingrese el numero hasta tres cifras"))
 
-if num1 >= 1000:
+if num1 >= 1000: #Se coloca una condicion por si se ingresa un valor que no es correcto, si se coloca uno de 4000, la condicion termina ahi, ya que no se cumple.
 	print("No se permiten numeros mayores a cuatros cifras,intente de nuevo")
 else:
-	if num1 >= 100:
+	if num1 >= 100: #Si se cumple funcion esta va a terminar aqui, diciendo que tiene 3 cifras
 		print("Tiene tres cifras")
 
-	else:
+	else: #Si no se cumple la condicion, esta se para a la ultima condicion
 		if num1 >= 10:
 			print("Tiene dos cifras")
 		else:

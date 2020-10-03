fce66b128adcb6d4dc57147335a9fc4a01904442
Itachi_Uchiha <itachi_uchiha@pop-os.localdomain>
2020-10-03 18:01:08 -0500

    Se modifico el problema 1

--- Páginasdiferentes/problema1.py
+++ Páginasdiferentes/problema1.py
@@ -1,1 +1,11 @@
-#PRoblema 1
+
+
+
+## Escribir un problema que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no
+
+edad_usuario = int(input("Ingrese su edad verdadera: \n"))
+if edad_usuario >= 18:
+ 	print("Es mayor de edad el usuario: \n")
+
+else:
+	print("No es mayor de edad")

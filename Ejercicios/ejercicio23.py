f582405b5d89334c5f8cadcfa14168f6b7146770
def depresion{} <depresion@pop-os.localdomain>
2020-10-26 22:03:56 -0600

    Ejercio23 con dos funciones, 1 completa y otra básica

--- Ejercicios/ejercicio23.py
+++ Ejercicios/ejercicio23.py
@@ -17,21 +17,26 @@ numero3 = int(input("Ingrese el tercer valor: \n"))
 if numero3 > numero1 or numero3 > numero2:
     if numero3 > numero1 and numero3 > numero2:
         print("El numero 3 es el mayor") """
+def ver_Numero(): # Funcion para determinar cual numero es el menor
+    if numero1 > numero2 and numero1 > numero3:
+        if numero2 < numero3:
+            print("El numero 2 es el menor")
+        else:
+            print("El numero 3 es el menor ")
 
-if numero1 > numero2 and numero1 > numero3:
-    if numero2 < numero3:
-        print("El numero 2 es el menor")
-    else:
-        print("El numero 3 es el menor ")
+    if numero2 > numero1 and numero2 > numero3:
+        if numero1 < numero3:
+            print("El numero 1 es el menor")
+        else:
+            print("El numero 3 es el numero menor ")
 
-if numero2 > numero1 and numero2 > numero3:
-    if numero1 < numero3:
-        print("El numero 1 es el menor")
-    else:
-        print("El numero 3 es el numero menor ")
+    if numero3 > numero1 and numero3 > numero2:
+        if numero2 < numero1:
+            print("El numero 2 es el menor")
+        else:
+            print("El numero 1 es el numero menor ")
 
-if numero3 > numero1 and numero3 > numero2:
-    if numero2 < numero1:
-        print("El numero 2 es el menor")
-    else:
-        print("El numero 1 es el numero menor ")
+def basica(): #Funcion en la que solo se sabe que alguno de ellos es menor 
+    if numero1 < 10 or numero2 < 10 or numero3 < 10:
+        print("Alguno de estos numeros es menor a 10")
+ver_Numero()

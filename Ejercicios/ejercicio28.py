2b49b1ca383272fa56cb02af069a8560ce8708bd
D1egoS4nchez <diegosanchezrmrez@gmail.com>
2020-11-02 19:55:13 -0600

    Ejercicio28

--- Ejercicios/ejercicio28.py
+++ Ejercicios/ejercicio28.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def mientras_funcion(x):
-    while x<=100:
+def mientras_funcion(x): #Se inicializa la variable x
+    while x<=100: #Funcion while "MIentras x sea menor o igual a 100 ejecuta este bloque de codigo"
         print(x)
-    return x=x+1
+        x = x + 1
 
-mientras_funcion(2)
+#mientras_funcion(99)
+
+def mientras_menos50(x): #Se inicializa la variable x
+    while x<=0: #Funcion while "MIentras x sea menor o igual a 100 ejecuta este bloque de codigo"
+        print(x)
+        x = x + 1
+mientras_menos50(-43)

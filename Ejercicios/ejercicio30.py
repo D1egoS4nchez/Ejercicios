6e7659952cc3ef16e46104e3d68261ade177baed
D1egoS4nchez <diegosanchezrmrez@gmail.com>
2020-11-02 20:37:46 -0600

    Ejercicio 30

---
+++ Ejercicios/ejercicio30.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def suma_while():
+    x = 1
+    suma = 0
+    while x<=10:
+        valor = int(input("Ingrese el numero que va a sumar: \n"))
+        suma = suma + valor
+        x = x + 1
+    promedio = suma / 10
+    print("La suma de los 10 valores que ingreso es: \n", suma)
+    print("\t")
+    print("El promedio es de: \t", promedio)
+
+suma_while()

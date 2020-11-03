22e4215553309a745ae098d4ed9684cc5826c47d
D1egoS4nchez <diegosanchezrmrez@gmail.com>
2020-11-02 20:29:47 -0600

    Ejercicio29

---
+++ Ejercicios/ejercicio29.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+n = int(input("Ingrese el valor que quiere su secuencia: \n"))
+def valor_personalizado(x):
+
+    while x<=n:
+        print(x)
+        x=x+1
+valor_personalizado(5)

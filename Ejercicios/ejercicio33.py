127c88530bf354136feeace2b49a44285a1c9a70
D1egoS4nchez <diegosanchezrmrez@gmail.com>
2020-11-03 07:58:16 -0600

    Ejercicio33

---
+++ Ejercicios/ejercicio33.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+x = 1
+
+def alturas():
+    suma_alturas = 0
+    n = int(input("Ingrese cuantas personas quiere ingresar: \n"))
+    while x<=n:
+        alturas_personas = int(input("Ingrese la altura en cms: \n"))
+        suma_alturas = suma_alturas + alturas_personas
+    promedio_altura = suma_alturas / n
+    print("El promedio de alturas de las personas ingresadas es de: \t", promedio_altura)
+
+alturas()

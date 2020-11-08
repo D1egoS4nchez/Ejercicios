fae0ea5eccbce702c260be26a5ec5bd7f8f8af44
D1egoS4nchez <diegosanchezrmrez@gmail.com>
2020-11-08 17:01:01 -0600

    No se dice nada, una disculpa lic

--- Ejercicios/ejercicio33.py
+++ Ejercicios/ejercicio33.py
@@ -3,13 +3,12 @@
 
 x = 1
 
-def alturas():
-    suma_alturas = 0
-    n = int(input("Ingrese cuantas personas quiere ingresar: \n"))
-    while x<=n:
-        alturas_personas = int(input("Ingrese la altura en cms: \n"))
-        suma_alturas = suma_alturas + alturas_personas
-    promedio_altura = suma_alturas / n
-    print("El promedio de alturas de las personas ingresadas es de: \t", promedio_altura)
 
-alturas()
+suma_alturas = 0
+n = int(input("Ingrese cuantas personas quiere ingresar: \n"))
+while x<=n:
+    alturas_personas = int(input("Ingrese la altura en cms: \n"))
+    suma_alturas = suma_alturas + alturas_personas
+    x = x + 1
+promedio_altura = suma_alturas / n
+print("El promedio de alturas de las personas ingresadas es de: \t", promedio_altura)

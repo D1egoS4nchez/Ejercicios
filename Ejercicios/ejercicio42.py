379d001033d7948c06d65a695a8e87f0611cdbf5
Depresion <depresion@pop-os.localdomain>
2020-11-19 09:48:40 -0600

    Ejercicio42 con algunas notas

---
+++ Ejercicios/ejercicio42.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+# Imprimir todos los números impares que hay entre 1 y 100.
+
+for x in range(1,100,2):
+    print(x)
+
+
+""" La funcion range puede tener tres parametros:
+        1- El primer espacio es el que indica el valor de la variable x
+        2- El segundo valor es el valor final o al que se quiere llegar
+
+        3- El tercer parametro indica cuanto se incrementa cada vuelta de la variablee, en este caso será x
+
+        En este ejemplo, el primer parametro recibe el valor de 1, el segundo parametro que corresponde al valor que queremos llegar o hasta el que tiene que parametro
+        le da un valor de 100, y el ultimo parametro que es el tercer parametro que como dice, este indica cuanto se incrementa cada vuelta de la variable """
+
+"""
+
+1 - Como aqui se puede ver aqui, es el valor inicia que es 1 que se puso en el parametro 1
+3      - En la segunda vuelta se incrementa 2 
+5
+7
+9
+11
+13
+15
+17
+19
+21
+23
+25
+27
+29
+31
+33
+35
+37
+39
+41
+43
+45
+47
+49
+51
+53
+55
+57
+59
+61
+63
+65
+67
+69
+71
+73
+75
+77
+79
+81
+83
+85
+87
+89
+91
+93
+95
+97
+99
+"""

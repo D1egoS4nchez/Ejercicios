340d75faa120b61a8bedfe68d7798b8b84115cf7
Depresion <depresion@pop-os.localdomain>
2020-11-18 18:00:35 -0600

    EJericicio36

--- Ejercicios/ejercicio36.py
+++ Ejercicios/ejercicio36.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+""" Mostrar los múltiplos de 8 hasta el valor 500. Debe aparecer en pantalla 8 - 16 - 24, etc. """
+
+i = 8
+
+while i <= 500:
+    print("------")
+    print(i)
+    i += 8

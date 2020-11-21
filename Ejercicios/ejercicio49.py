df64e6b6b8fdebb1f9a1900048f7655f90a59f5e
D1egoS4nchez <sanchezamrezdiego@gmail.com>
2020-11-21 17:03:00 -0600

    Ejercicio 49

---
+++ Ejercicios/ejercicio49.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" Desarrollar un programa que muestre la tabla de multiplicar del 5 (del 5 al 50) """
+
+multiplo = 5
+for f in range(1,11):
+    print(f*multiplo)

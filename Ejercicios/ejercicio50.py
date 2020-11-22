3c3a0894b8a430fa97fc19b9c4ae92b366c416d5
D1egoS4nchez <sanchezamrezdiego@gmail.com>
2020-11-21 19:37:05 -0600

    Ejercicio con algunas adiciones
    En este se busca que se si no se ingresa el numero correcto se repita la entrada
    y si esta esta en el rango debido, que siga en la siguiente linea de codigo

--- Ejercicios/ejercicio50.py
+++ Ejercicios/ejercicio50.py
@@ -6,15 +6,15 @@
 Ejemplo: Si ingreso 3 deberá aparecer en pantalla los valores 3, 6, 9, hasta el 36."""
 
 
-numero = int(input("Ingrese el numero del 1 al 10 que quiere multiplicar: \n"))
+"""numero = int(input("Ingrese el numero del 1 al 10 que quiere multiplicar: \n"))
 print("-------")
 for f in range(1,11):
     multiplicacion = numero * f
 
     print(multiplicacion)
+"""
 
-
-"""numero = int(input("Ingrese el numero del 1 al 10 que quiere multiplicar: \n"))
+numero = int(input("Ingrese el numero del 1 al 10 que quiere multiplicar: \n"))
 if numero in range(10):
     regreso = True
 else:
@@ -23,4 +23,9 @@ else:
 
 while regreso == False:
 
-    numero = int(input("Ingrese el numero del 1 al 10 que quiere multiplicar: \n"))"""
+    numero = int(input("Ingrese el numero del 1 al 10 que quiere multiplicar: \n"))
+
+for f in range(1,11):
+    multiplicacion = numero * f
+
+    print(multiplicacion)

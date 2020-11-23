63a17982727cc9728c82e0ebb6d9f563dada5561
D1egoS4nchez <sanchezamrezdiego@gmail.com>
2020-11-23 10:13:51 -0600

    Ejercicio 62

---
+++ Ejercicios/ejercicio62.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+nombre = input("Ingrese su nombre: \n")
+print("Primer caracter")
+
+print(nombre[0])
+print("Cantidad de letras de nombre: {}".format(len(nombre)))

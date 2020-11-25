c2d6308a8d3ca943993741e7067f07eda7edc3a0
D1egoS4nchez <sanchezamrezdiego@gmail.com>
2020-11-25 17:57:14 -0600

    Ejercicio70

---
+++ Ejercicios/ejercicio70.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+
+"""Definir una lista por asignación que almacene los nombres de los primeros cuatro meses de año. Mostrar el primer y último elemento de la lista solamente."""
+
+nombres_meses = ["Enero", "Febrero","Marzo","Abril"]
+
+print(nombres_meses[0]) # se muestra enero
+print(nombres_meses[3]) # se muestra abril

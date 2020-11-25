e1ac12263554be23f04b3b9c9d811fef57a31af7
D1egoS4nchez <sanchezamrezdiego@gmail.com>
2020-11-25 17:05:42 -0600

    Ejercicio 68

--- Ejercicios/ejercicio68.py
+++ Ejercicios/ejercicio68.py
@@ -3,4 +3,16 @@
 
 
 """Solicitar el ingreso de una clave por teclado y almacenarla en una cadena de caracteres.
-    Controlar que el string ingresado tenga entre 10 y 20 caracteres para que sea válido, en caso contrario mostrar un mensaje de error."""
+Controlar que el string ingresado tenga entre 10 y 20 caracteres para que sea válido, en caso contrario mostrar un mensaje de error."""
+
+
+clave_ingreso = input("Ingrese su  clave")
+
+cantidad = len(clave_ingreso)
+print(cantidad)
+
+if cantidad >= 10 and cantidad <= 20:
+    print("Es validad")
+
+else:
+    print("No es valida")

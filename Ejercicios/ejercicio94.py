c09f364e45ef20c1afdcddf6eada219149b95f8a
Diego Sanchez <sanchezrmrezdiego@gmail.com>
2020-12-10 21:48:35 -0600

    Ejercicio 94 con algo de modificacion

--- Ejercicios/ejercicio94.py
+++ Ejercicios/ejercicio94.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 """Crear y cargar en un lista los nombres de 5 países y en otra lista paralela la cantidad de habitantes del mismo.
 Ordenar alfabéticamente e imprimir los resultados. Por último ordenar con respecto a la cantidad de habitantes (de mayor a menor) e imprimir nuevamente."""
 

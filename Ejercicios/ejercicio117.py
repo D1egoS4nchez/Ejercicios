e601f4ececb100a6ad9417594a43df3b1b384395
Diego Sanchez <sanchezrmrezdiego@gmail.com>
2020-12-11 18:38:16 -0600

    Uwuw

---
+++ Ejercicios/ejercicio117.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+def mostrar_mayor(v1,v2,v3):
+
+    print("El mayor de los tres numeros es ")
+
+    if v1>v2 and v1>v3:
+        print(v1)
+    else:
+        if v2>v3:
+            print(v2)
+        else:
+            print(v3)
+
+def cargar():
+    valor1 = int(input("Ingrese el primero valor: "))
+    valor2 = int(input("Ingrese el segundo valor: "))
+    valor3 = int(input("Ingrese el tercer valor: "))
+    mostrar_mayor(valor1, valor2, valor3)
+
+cargar()

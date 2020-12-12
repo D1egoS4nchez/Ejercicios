75dc8cad56ab4f603d65f9c995d52896d836dea8
Diego Sanchez <sanchezrmrezdiego@gmail.com>
2020-12-11 18:38:31 -0600

    Ejercicio 120 no terminado

---
+++ Ejercicios/ejercicio120.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3  
+
+
+"""Confeccionar una función que reciba tres enteros y los 
+muestre ordenados de menor a mayor. En otra función solicitar la carga de 3 enteros por teclado y proceder a llamar a la primer función definida. """
+
+
+def comparar(nm1, nm2, nm3):
+    

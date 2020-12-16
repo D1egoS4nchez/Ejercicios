caa1052956708f0ba40550da27239b7a9ce40e96
D1egoS4nchez <diegosanchezrmrez@gmail.com>
2020-12-16 17:28:42 -0600

    Se completo el ejercicioa
    En este ejercicio lo que se hizo fue tal cual primero retornar la frase que se guardo en una variable, despues en la segunda funcion, se hizo
    una variable en el que se guardo la frase y de una vez se inicializo la funcion para que lanzará lo que tuviera adentro

--- Ejercicios/ejercicio126.py
+++ Ejercicios/ejercicio126.py
@@ -2,3 +2,27 @@
 #-*- coding: utf-8 -*-
 
 
+""" Plantear una función que reciba un string en mayúsculas o minúsculas y retorne la cantidad de letras 'a' o 'A'. """
+
+
+def recibir():
+    sentece = input("Ingrese el string: \n")
+    print("Esta es su frase: \n {}".format(sentece))
+    return sentece
+def checar():
+
+    
+
+    frase = recibir()
+    retorno_numero = 0
+    for x in range(len(frase)):
+        if frase[x] == "a" or frase[x] == "A":
+            retorno_numero += 1
+
+    print("Su frase que ingreso tiene este numero de a (minusculass) y A(mayusculas) {} : .format(retorno_numero))
+
+#bloque principal
+
+checar()
+
+

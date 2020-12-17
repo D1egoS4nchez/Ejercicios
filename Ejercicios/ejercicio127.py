c064fa811ee368971ca9a1da319d640f73ada09b
Diego Sanchez <sanchezrmrezdiego@gmail.com>
2020-12-17 17:33:21 -0600

    Se esta conmentando cada linea de codigo

--- Ejercicios/ejercicio127.py
+++ Ejercicios/ejercicio127.py
@@ -3,10 +3,19 @@
 
 #Funciones parametros lista
 
+
+#Se hace una funcion para sumar toda la lista
 def sumarizar(lista):
+    #Se inicializa una variable para guardar el resultado en cada vuelta
     suma=0
+
+    #Se hace un for para iterar en la lista
     for x in range(len(lista)):
+
+        #En cada vuelta se suma la posicion 
         suma=suma+lista[x]
+
+    #Se devuelve lo que es la variable
     return suma
 
 
@@ -20,7 +29,11 @@ def mayor(lista):
 
 def menor(lista):
     men=lista[0]
+
+    #Se hace un bucle for pero la primera posicion en la queinicia es uno y se toma lo largo el largo 
     for x in range(1,len(lista)):
+
+        #Hacemos una condicion 
         if lista[x]<men:
             men=lista[x]
     return men

d779a10b23f9bfbf9c8804d10d7ac5f38d6536ff
D1egoS4nchez <diegosanchezrmrez@gmail.com>
2020-12-18 22:42:29 -0600

    Ejercicio 135 aun en proceso

--- Ejercicios/ejercicio135.py
+++ Ejercicios/ejercicio135.py
@@ -10,3 +10,16 @@ Desarrollar las siguientes funciones y llamarlas desde el bloque principal:
 3) Cuántos tienen un sueldo superior a $4000.
 4) Retornar el promedio de los sueldos.
 5) Mostrar todos los sueldos que están por debajo del promedio. """
+
+def cargar_lista():
+    lista = []
+    for x in range(10):
+        valorsto = int(input("Ingrese el valo que quiere cara de pija: "))
+        lista.append(valorsto)
+    return lista
+
+def impresion():
+    print(cargar_lista())
+
+#MAIN BLOCK
+impresion()

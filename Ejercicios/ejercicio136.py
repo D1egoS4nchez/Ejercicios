bd97837ae9894363e3950732573198b70523ca85
Diego Sanchez <sanchezrmrezdiego@gmail.com>
2020-12-19 22:41:28 -0600

    Aun no se completa xD

---
+++ Ejercicios/ejercicio136.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+""" Desarrollar una aplicación que permita ingresar por teclado los nombres de 5 artículos y sus precios.
+Definir las siguientes funciones:
+1) Cargar los nombres de articulos y sus precios.
+2) Imprimir los nombres y precios.
+3) Imprimir el nombre de artículo con un precio mayor
+4) Ingresar por teclado un importe y luego mostrar todos los artículos con un precio menor igual al valor ingresado. """
+
+

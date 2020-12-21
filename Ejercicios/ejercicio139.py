683e7e66fc12aa9cb031dc2f622a9da692a9ce30
D1egoS4nchez <diegosanchezrmrez@gmail.com>
2020-12-21 12:24:10 -0600

    Ejercicio139, solo tiene las indicaciones

---
+++ Ejercicios/ejercicio139.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+
+"""Confeccionar una función que reciba entre 2 y 5 enteros. 
+La misma nos debe retornar la suma de dichos valores. Debe tener tres parámetros por defecto."""

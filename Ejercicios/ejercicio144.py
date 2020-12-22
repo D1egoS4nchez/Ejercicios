9df6efcded74fa492f704a7e1faa115adac9f006
D1egoS4nchez <diegosanchezrmrez@gmail.com>
2020-12-22 12:16:22 -0600

    No se ha iniciado porque estamos re pete y escuchando Hopeless Romantic

---
+++ Ejercicios/ejercicio144.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+"""Confeccionar una función que reciba una serie de edades y me retorne la cantidad que 
+son mayores o iguales a 18 (como mínimo se envía un entero a la función)"""
+
+print("Caca")
+

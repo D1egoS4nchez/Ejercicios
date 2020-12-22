7e845f6635e018dc4070a82d35681b496bd753c5
D1egoS4nchez <diegosanchezrmrez@gmail.com>
2020-12-22 12:49:40 -0600

    Ejercicio 144 completado

--- Ejercicios/ejercicio144.py
+++ Ejercicios/ejercicio144.py
@@ -4,5 +4,15 @@
 """Confeccionar una función que reciba una serie de edades y me retorne la cantidad que 
 son mayores o iguales a 18 (como mínimo se envía un entero a la función)"""
 
-print("Caca")
+def cantidad_mayores(edad1, *edades):
+    cantidad = 0
+    if edad1>18:
+        cantidad += 1
+    for x in range(len(edades)):
+        if edades[x] >= 18: 
+            cantidad += 1
+    return cantidad
 
+
+
+print("La cantidad de personas que son mayores de edad son  {}".format(cantidad_mayores(23,17,6,19,16)))

2fd4eb523ef406c9a1b1a8b0b624210b49cf9659
D1egoS4nchez <diegosanchezrmrez@gmail.com>
2020-12-22 16:54:12 -0600

    Tuplas

--- Ejercicios/ejercicio146.py
+++ Ejercicios/ejercicio146.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
+
+tupla = (1,2,3)
+fecha=(25, "Diciembre", 2016)
+punto=(10, 2)
+persona=("Rodriguez", "Pablo", 43)
+print(tupla)
+print(fecha)
+print(punto)
+print(persona)

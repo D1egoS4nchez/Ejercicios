ba14f3e6c840ec13cf194be5fa138ae5161bb824
D1egoS4nchez <diegosanchezrmrez@gmail.com>
2020-12-22 12:16:54 -0600

    Ejercicio 143 completado ;)

---
+++ Ejercicios/ejercicio143.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+#-*-coding: utf-8 -*- 
+
+"""Confeccionar una función que reciba entre 2 y n (siendo n = 2,3,4,5,6 etc.) valores enteros, 
+retornar la suma de dichos parámetros."""
+
+def sumar(v1,v2,*lista):
+    suma=v1+v2
+    for x in range(len(lista)):
+        suma = suma + lista[x]
+    return suma
+
+#main block
+
+print("La suma de 1+2 es")
+print(sumar(1,2))
+print("La suma de 1+2+3+4")
+print(sumar(1,2,3,4))
+print("La suma de 1+2+3+4+5+6+7+8+9 es ")
+print(sumar(1,2,3,4,5,6,7,8,9))
+

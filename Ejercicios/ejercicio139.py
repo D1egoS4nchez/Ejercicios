93b3d9d0d99a5181219345148e47d96c52f77d07
D1egoS4nchez <diegosanchezrmrez@gmail.com>
2020-12-21 21:33:28 -0600

    XDA

--- Ejercicios/ejercicio139.py
+++ Ejercicios/ejercicio139.py
@@ -5,4 +5,9 @@
 """Confeccionar una función que reciba entre 2 y 5 enteros. 
 La misma nos debe retornar la suma de dichos valores. Debe tener tres parámetros por defecto."""
 
-def sumar(
+def sumar(v1=0, v2=0, v3=0, v4=4, v5=7):
+    suma = v1+v2+v3+v4+v5
+    return suma
+
+
+print(sumar(5,3,1,5))

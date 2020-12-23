7fa0dd9d9a2656f521c12988e80b718bd4e2b074
D1egoS4nchez <diegosanchezrmrez@gmail.com>
2020-12-23 15:10:26 -0600

    Ejercicio 149 completado pero sin comentar

--- Ejercicios/ejercicio149.py
+++ Ejercicios/ejercicio149.py
@@ -10,13 +10,27 @@ def cargar_valores():
     lista = []
     for x in range(5):
         valor = int(input("Ingrese el valor: "))
-        list.append(valor)
+        lista.append(valor)
     return lista
 
 def mayor_menor(lis):
-     
+    may = lis[0]
+    men = lis[0]
+    for x in range(len(lis)):
+        if lis[x] > may:
+            may = lis[x]
+        if lis[x] < men:
+            men = lis[x]
+    return (may, men)
 
 
 #MAIN BLOCK
+leste = cargar_valores()
+#Lo que se retorno se guardo en una tupla
+tupla = mayor_menor(leste)
+print(tupla)
+mayor, menor = mayor_menor(leste)
 
+print("El numero que es mayor a todos es {}".format(mayor))
+print("El numero que es menor a todos es {}".format(menor))
 

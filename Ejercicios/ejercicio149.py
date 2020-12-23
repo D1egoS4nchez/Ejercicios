c2bfcd1506463d13c38010425729db311f92091b
D1egoS4nchez <diegosanchezrmrez@gmail.com>
2020-12-23 14:49:32 -0600

    Ejercicio 149 que trata de hacer iuna lista y despues convertirlas a tuplaswq

---
+++ Ejercicios/ejercicio149.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+""" Confeccionar un programa con las siguientes funciones:
+1)Cargar una lista de 5 enteros.
+2)Retornar el mayor y menor valor de la lista mediante una tupla.
+Desempaquetar la tupla en el bloque principal y mostrar el mayor y menor. """
+
+def cargar_valores():
+    lista = []
+    for x in range(5):
+        valor = int(input("Ingrese el valor: "))
+        list.append(valor)
+    return lista
+
+def mayor_menor(lis):
+     
+
+
+#MAIN BLOCK
+
+

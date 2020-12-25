1d9c66c5e8adae8f5a6d81a13b47c91cce84d8df
D1egoS4nchez <diegosanchezrmrez@gmail.com>
2020-12-25 08:43:19 -0600

    Ejercicio 156, aun sin completar

---
+++ Ejercicios/ejercicio156.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3  
+#-*- coding: utf-8 -+-
+
+""" Almacenar en una lista de 5 elementos las tuplas con el nombre de empleado y su sueldo.
+Implementar las funciones:
+1) Carga de empleados.
+2) Impresión de los empleados y sus sueldos.
+3) Nombre del empleado con sueldo mayor.
+4) Cantidad de empleados con sueldo menor a 1000. """
+
+

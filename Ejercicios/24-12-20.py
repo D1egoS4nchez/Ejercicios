dc949873841427a5b8ace9920d877e5a67a41fcc
D1egoS4nchez <diegosanchezrmrez@gmail.com>
2020-12-24 21:40:24 -0600

    Recordatorio

---
+++ Ejercicios/24-12-20.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3  
+#-*- coding: utf-8 -*-
+
+x = 0
+
+while x == 0:
+    for z in range(20):
+        print("Esto es un nuevo año entonce hay que iniciar a trabajar y progresar, recuerda antes del 18 de enero terminar y repasar el libro de trigonometria y geometria analitica")
+    x += 1

00e8f7c949fae2c2cfd2768c6f05b0a9df041074
Diego Sanchez <sanchezrmrezdiego@gmail.com>
2020-12-27 09:21:31 -0600

    ANtes de fusionar

--- Ejercicios/ejercicio156.py
+++ Ejercicios/ejercicio156.py
@@ -20,3 +20,23 @@ def cargar():
 def imprimir(empleados):
     print("Listado de los nombres de empleados y sus sueldos")
     
+
+def mayor_sueldo(empleados):
+    empleado = empleados[0]
+    for emp in empleados:
+        if emp[1]>empleado[1]:
+            empleado=emp
+    print("Empleado con mayor sueldo:",empleado[0],"su sueldo es",empleado[1])
+
+def sueldos_menor1000(empleados):
+    cant = 0
+    for empleado in empleados:
+        if empleado[1]<1000:
+            cant=+1
+    print("Cantidad de empleados con sun sueldo menor a 1000 son:",cant)
+
+    # bloque principal
+empleados=cargar()
+imprimir(empleados)
+mayor_sueldo(empleados)                        
+sueldos_menor1000(empleados)

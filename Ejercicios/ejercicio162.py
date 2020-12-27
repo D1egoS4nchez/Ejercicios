86e78f7d8e93e0e32ac6d9f227cb65a3d312fdde
Diego Sanchez <sanchezrmrezdiego@gmail.com>
2020-12-27 13:41:13 -0600

    Ejercicio162 sin terminar y sin comentar

---
+++ Ejercicios/ejercicio162.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3  
+#-*- coding: utf-8 -*-
+
+""" Crear un diccionario en Python que defina como clave el número de documento de una persona y como valor un string con su nombre.
+Desarrollar las siguientes funciones:
+1) Cargar por teclado los datos de 4 personas.
+2) Listado completo del diccionario.
+3) Consulta del nombre de una persona ingresando su número de documento. """
+
+
+def cargar():
+    diccionario = {}
+    whil = "s"
+    while whil == "s" or "S" or "si":
+        num_doc = int(input("Ingrese el numero de documento de la persona: "))
+        nombre = input("Ingrese el nombre de una persona")
+        diccionario[num_doc]=nombre
+        whil = input("Quiere ingresar otro numero y nombre?: [s/n]")
+
+    return diccionario
+
+
+
+#MAIN BLOCK
+
+dic = cargar()

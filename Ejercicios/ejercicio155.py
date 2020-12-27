ff3c043c29ab3e9b9c152be4d133887266d27424
Diego Sanchez <sanchezrmrezdiego@gmail.com>
2020-12-27 09:47:56 -0600

    Aun no se termina de comentar

--- Ejercicios/ejercicio155.py
+++ Ejercicios/ejercicio155.py
@@ -8,18 +8,25 @@ Luego en otras funciones:
 3) Mostrar la suma de todas sus componentes.
 Utilizar la nueva sintaxis de for vista en este concepto. """
 
-
+#Funcion para cargar la lista
 def cargar():
+    #Se hace una lista vacia
     lista = []
+    #Bucle para agregar un valor en la variable num cada vuelta
     for x in range(5):
         num = int(input("Ingrese un valor: "))
+        #En cada vuelta se agrega el valor anterior en la lista vacia
         lista.append(num)
+    #Se retorna la lista con los numeros ya ingresafd
     return lista
 
-
+#Funcion para imprimir cada elemento de la lista, con el parametro que en este caso es la lista que se devolvio anteriormente en la funcion cargar                              
 def imprimir(lista):
+
     print("Lista completa")
+    #La palabra elemento es igual a poner un x, en este caso lo no es necesario poner la lista y el elemento
     for elemento in lista:
+
         print(elemento, end="-")
     print("\n")
 

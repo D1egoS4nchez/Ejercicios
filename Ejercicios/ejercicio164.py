85f9b92a01cb3f7692f1131926a63c4e021f5da0
Diego Sanchez <sanchezrmrezdiego@gmail.com>
2020-12-30 11:55:04 -0600

    Ejercicio164 sin completar los comentarios

--- Ejercicios/ejercicio164.py
+++ Ejercicios/ejercicio164.py
@@ -19,11 +19,16 @@ def cargar():
     continua1 = "s"
     #Hacemos un while para ingresar datos en el diccionario
     while continua1 == "s":
-        #
+        #Guardamos un valor en la variable
         fecha = input("Ingrese la fecha con formaton dd/mm/aa: ")
+        #Hacemos un puntero para hacer otro while, en este caso es un bucle dentro de uno para ingresar otro tipo de informacion
         continua2 = "s"
+        #Hacemos una lista vacia
         lista = []
+        #Hacemos otro while para guardar los valores en la lista y no se tenga problemas x
+        #Hacemos un while para continuar en el bucle
         while continua2 == "s":
+
             hora = input("Ingrese la hora con formato hh:mm ")
             actividad = input("Ingrese la descripcion de la actividad: ")
             lista.append((hora.actividad))
@@ -32,6 +37,7 @@ def cargar():
         continua1 = input("Ingresa otra fecha [s/n]: ")
     return agenda
 
+
 def imprimir(agenda):
     print("Listado completa de la agenda")
     for fecha in agenda:
@@ -39,9 +45,10 @@ def imprimir(agenda):
         for hora,actividad in agenda[fecha]:
             print(hora,actividad)
 
-
+#Podemos hacer un while para repetir esta funcion con un puntero
 
 def consulta_fecha(agenda):
+    #Ingresamos un valor para buscar en el diccionario
     fecha = int(input("Ingrese la fecha que desea consultar: "))
     if fecha in agenda:
         for hora,actividad in agenda[fecha]:
@@ -51,7 +58,13 @@ def consulta_fecha(agenda):
 
 
     
-#MAIN
-agenda = cargar()
-imprimir(agenda)
-consulta_fecha(agenda)
+def main():
+
+    agenda = cargar()
+    imprimir(agenda)
+    point = "s"
+    while point == "s" or point == "S" or point == "si" or point == "SI":
+        consulta_fecha(agenda)
+        point = input("¿Quiere buscar otra fecha? [s/n]")
+
+main()

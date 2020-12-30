4e9b0d41399be047147271f897b67287feb9d71b
Diego Sanchez <sanchezrmrezdiego@gmail.com>
2020-12-30 16:10:02 -0600

    Se termino de comentar, en este se pudo ver como se contemplaron dos bucles while y como se desempaqueto el valor del diccionario que eran las lista

--- Ejercicios/ejercicio164.py
+++ Ejercicios/ejercicio164.py
@@ -27,22 +27,34 @@ def cargar():
         lista = []
         #Hacemos otro while para guardar los valores en la lista y no se tenga problemas x
         #Hacemos un while para continuar en el bucle
+
+        #Este while permitira ingresar los valores del diccionario o de la clave pero en una lista 
         while continua2=="s":
             hora=input("Ingrese la hora de la actividad con formato hh:mm ")
             actividad=input("Ingrese la descripcon de la actividad:")
+            #Agregamos los valores anteriormente ingresados en una lista
             lista.append((hora,actividad))
+            #Hacemos un while para agregar mas actividades, y con este podemos agregar más
             continua2=input("Ingresa otra actividad para la misma fecha[s/n]:")
+        #Agregamos los valores en el diccionario, la lista como el valor de la clave que es la variable fecha
         agenda[fecha]=lista
+        #Con este puntero podemos repetir el bloque principal que es agregar una fecha
         continua1=input("Ingresa otra fecha[s/n]:")
-     
+     #Retornamos el diccionario para guardarlo en una variable o imprimirlo
     return agenda
 
-
+#Funcion para imprimir el diccionario que retornamos en la funcion pasada, en esta ponemos como parametro el valor agenda que es el diccionario
 def imprimir(agenda):
+    #Imprime un mensaje
     print("Listado completa de la agenda")
+    #Iteramos en el diccionario con la clave que es la fecha
     for fecha in agenda:
+        #Imprime lo que es la clave del diccionario
         print("Para la fecha: {}".format(fecha))
+        #Hacemos otro for para iterar en la lista o el valor que contiene anidada una lista para asi desempaquetarlos o entrar en el valor del diccionario
         for hora,actividad in agenda[fecha]:
+            #En este ya se tiene desempaquetado lo que es la lista o el valor en el diccionario, depues de la parte agenda ponemos lo que es la clave que seria en dicci[valor], esto para no meternos en la parte incorrecta 
+            #y que imprima correctamente lo que son los valores de la lista que esta adentro de la seccion valor del diccionario 
             print(hora,actividad)
 
 #Podemos hacer un while para repetir esta funcion con un puntero
@@ -50,11 +62,15 @@ def imprimir(agenda):
 def consulta_fecha(agenda):
     #Ingresamos un valor para buscar en el diccionario
     fecha = int(input("Ingrese la fecha que desea consultar: "))
+    #Si esta el valor que ingresamos en la variable, esta imprimira lo que son los valores
     if fecha in agenda:
+        #Asi que iteramos en el valor del diccionario que es poniendo los dos valores 
         for hora,actividad in agenda[fecha]:
+            #Imprimimos los valores de las posiciones
             print(hora,actividad)
-        else:
-            print("No hay actividades agendadas para dicha fecha")
+    #Si no se cumple la condicion anterior esta mostrará un mensaje de que no esta disponible o no fue ingresada en el diccionario 
+    else:
+        print("No hay actividades agendadas para dicha fecha")
 
 
     

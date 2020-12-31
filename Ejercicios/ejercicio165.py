d5f4b80e952c663cd9e893e613e49bcfb8c29763
Diego Sanchez <sanchezrmrezdiego@gmail.com>
2020-12-30 22:16:09 -0600

    Se agregaron algunos comentarios

--- Ejercicios/ejercicio165.py
+++ Ejercicios/ejercicio165.py
@@ -9,15 +9,19 @@ Crear las siguientes funciones:
 3) Consulta de un alumno por su dni, mostrar las materias que cursa y sus notas. 
 """
 
-
+#FUNCION PARA CARGAR LOS VALORES EN EL DICCIONARIO, en este caso sera lo que es el numero del estudiante como clave y como valor, el nombre de la materia y la nota que saco en ella
 def charge():
 
-    
+    #Diccionario vacio  #Diccionario vacio   
     curricular = {}
+    #   Pointer para hacerl el while
     pointer1 = "s"
     while pointer1 == "s" or pointer1 == "si" or pointer1 == "S" or pointer1 == "SI":
+        #Ingresamos el valor para la clave del diccionario
         num_doc = int(input("Ingrese el numero del documento del alumno: "))
+        #Hacemos una lista vacia para agregar los valores como tuplas en una lista
         lista = []
+        #Hacemos otro puntero para agregar dos valores
         pointer2 = "s"
         while pointer2 == "s" or pointer2 == "S" or pointer2 == "si" or pointer2 == "SI":
             materia = input("Ingrese el nombre de la materia: ")

f84cd968f099a516f417fd09b65caf175032057a
Diego Sanchez <sanchezrmrezdiego@gmail.com>
2021-01-02 20:30:08 -0600

    Terminado pero el while no es muylogico

--- Ejercicios/ejercicio181.py
+++ Ejercicios/ejercicio181.py
@@ -10,3 +10,16 @@ El operador debe tratar de adivinar el número ingresado.
 Cada vez que ingrese un número mostrar un mensaje "Gano" si es igual al generado o "El número aleatorio es mayor" o "El número aleatorio es menor".
 Mostrar cuando gana el jugador cuantos intentos necesitó.
 """
+
+valor = random.randint(1,100)
+pointer = False
+s = "s"
+while pointer == False and s == "s":
+    n = int(input("Ingrese el valor: "))
+    if n == valor:
+        pointer = True
+    else:
+        pass
+    s = input("Quiere intentarlo otra vez?")
+print(valor)
+    

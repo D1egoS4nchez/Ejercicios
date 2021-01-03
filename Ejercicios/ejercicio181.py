a9158c7f78fbd611ecbf1c33e787080b96de8c64
Diego Sanchez <sanchezrmrezdiego@gmail.com>
2021-01-02 19:32:20 -0600

    Libreria Random, esta esta integrada en la biblioteca estandar de python#

---
+++ Ejercicios/ejercicio181.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3  
+#-*- coding: utf-8 -*-
+
+#Importamos la libreria random
+import random
+
+"""
+Confeccionar un programa que genere un número aleatorio entre 1 y 100 y no se muestre.
+El operador debe tratar de adivinar el número ingresado.
+Cada vez que ingrese un número mostrar un mensaje "Gano" si es igual al generado o "El número aleatorio es mayor" o "El número aleatorio es menor".
+Mostrar cuando gana el jugador cuantos intentos necesitó.
+"""

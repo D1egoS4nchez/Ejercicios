bc346d904f56e837ef3e818684f2b316c5a089d4
Diego <sanchezramrezdiego@gmail.com>
2021-01-04 17:07:46 -0600

    Ejercicio189 terminado pero no se ha comentado

--- Ejercicios/ejercicio189.py
+++ Ejercicios/ejercicio189.py
@@ -7,4 +7,26 @@ imprimir el valor del lado mayor y otro método que muestre si es equilátero o 
 """
 
 class Triangulo:
-    def
+    def inicializar(self):
+        lista = []
+        for x in range(3):
+            valor = float(input("Ingrese el valor del lado:"))
+            lista.append(valor)
+        self.lados = lista
+
+    def imprimir(self):
+        print(self.lados)
+
+    def checar(self):
+        if self.lados[0] == self.lados[1] and self.lados[0] == self.lados[2]:
+            print("Es equilatero")
+        else:
+            print("No es equilatero")
+
+def main():
+    lista_lados = Triangulo()
+    lista_lados.inicializar()
+    lista_lados.imprimir()
+    lista_lados.checar()
+
+main()

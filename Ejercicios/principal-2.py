95e450cedd9a0837761e6290d883b467dd44a56a
D1egoS4nchez <diegosanchezrmrez@gmail.com>
2021-01-03 18:32:30 -0600

    Funciones que se pueden importar
    En este caso lo que hacemos es hacer las funciones mayor y menor en el documento mayormenor.py , pero en el siguiente que va a ser el que
    ejecute las funciones, solo tomamos la funcion para ver el mayor haciendo el from e import

---
+++ Ejercicios/principal-2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+from mayormenor import mayor
+
+valor1 = int(input("Ingrese su primer valor:"))
+valor2 = int(input("Ingrese su segundo valor:"))
+print("El mayor de los dos numeros es {}".format(mayor(valor1,valor2)))

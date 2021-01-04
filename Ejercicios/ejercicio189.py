1d2f479f2725e6e98e3d6d8319cf37d1c26f2f78
Diego <sanchezramrezdiego@gmail.com>
2021-01-04 16:43:37 -0600

    Ejercicio189 no terminado

---
+++ Ejercicios/ejercicio189.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3  
+#-*- coding: utf-8 -*-
+
+"""
+Desarrollar un programa que cargue los lados de un triángulo e implemente los siguientes métodos: inicializar los atributos, 
+imprimir el valor del lado mayor y otro método que muestre si es equilátero o no. El nombre de la clase llamarla Triangulo.
+"""
+
+class Triangulo:
+    def

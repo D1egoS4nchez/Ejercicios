23dcd7edcb707374178a9e5dfc9e1c91f9b3680c
Diego <sanchezramrezdiego@gmail.com>
2021-01-05 09:38:03 -0600

    Ejercicio 193 terminado pero no se ha comentado
    SOlo se hicieron algunos arreglos y se agrego una funcion para que este mas estructurada

--- Ejercicios/ejercicio193.py
+++ Ejercicios/ejercicio193.py
@@ -17,42 +17,48 @@ class Operaciones:
 
         def suma():
             self.suma = self.valor_1 + self.valor_2
-            print("La suma de {} y {} es {}".format(self.valor_1,self.valor_2, suma))
+            #print("La suma de {} y {} es {}".format(self.valor_1,self.valor_2, suma))
         
 
         def resta():
             if self.valor_1 > self.valor_2:
                 self.resta =  self.valor_1 - self.valor_2
-                print("La resta de {} y {} resulta {}".format(self.valor_1,self.valor_2,self.resta))
+                #print("La resta de {} y {} resulta {}".format(self.valor_1,self.valor_2,self.resta))
             else:
                 self.resta = self.valor_2 - self.valor_1
-                print("La resta de {} y {} resulta {}".format(self.valor_2,self.valor_1,self.resta))
+                #print("La resta de {} y {} resulta {}".format(self.valor_2,self.valor_1,self.resta))
 
 
         def multiplicar():
             self.multi = self.valor_1 * self.valor_2
-            print("La multi de {} y {} resulta {}".format(self.valor_1,self.valor_2,self.multi))
+            #print("La multi de {} y {} resulta {}".format(self.valor_1,self.valor_2,self.multi))
     
 
         def division():
             if self.valor_1 > self.valor_2:
                 self.div = self.valor_1 / self.valor_2
-                print("La divsion de {} y {}, es {}".format(self.valor_1, self.valor_2, self.div))
+                #print("La divsion de {} y {}, es {}".format(self.valor_1, self.valor_2, self.div))
             else:
                 self.div = self.valor_2 / self.valor_1
-                print("La divsion de {} y {}, es {}".format(self.valor_2, self.valor_1, self.div))
+                #print("La divsion de {} y {}, es {}".format(self.valor_2, self.valor_1, self.div))
 
     
+        def iniciar():
+            suma()
+            resta()
+            multiplicar()
+            division() 
+        
+        iniciar()
 
-        suma()
-        resta()
-        multiplicar()
-        division()
-    
 
     def imprimir(self):
-        print(self.suma)
-        print(self.resta)
+        print("La suma es {}".format(self.suma))
+        print("La resta es {}".format(self.resta))
+        print("La multiplicaciones es {}".format(self.multi))
+        print("La division es {}".format(self.div))
+
+
 #MAIN BLOCK
 
 operacion_1 = Operaciones()

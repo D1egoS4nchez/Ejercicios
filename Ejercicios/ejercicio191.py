cdae23e0a5c096609fb4bb6c8be2041af8509ae9
Diego <sanchezramrezdiego@gmail.com>
2021-01-05 08:24:58 -0600

    Ejercicio 191 - Clases, metodos y atributos
    Falta de comentar

---
+++ Ejercicios/ejercicio191.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+"""
+Desarrollar una clase que represente un punto en el plano y tenga los siguientes métodos: inicializar los 
+valores de x e y que llegan como parámetros, imprimir en que cuadrante se encuentra dicho punto 
+(concepto matemático, primer cuadrante si x e y son positivas, si x<0 e y>0 segundo cuadrante, etc.)
+"""
+
+class Punto:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+
+    def imprimir(self):
+        print("El valor de x es {} y el valor de 'y' es {}".format(self.x,self.y))
+
+    def imprimir_cuadrante(self):
+        if self.x>0 and self.y>0:
+            print("Primer cuadrange")
+        else:
+            if self.x<0 and self.y>0:
+                print("Segundo cuadrante")
+            else:
+                if self.x<0 and self.y<0:
+                    print("Tercer cuadrante")
+                else:
+                    if self.x>0 and self.y<0:
+                        print("Cuarto cuadrante")
+
+
+#MAIN BLOCK
+
+punto1 = Punto(10,-2)
+punto1.imprimir()
+punto1.imprimir_cuadrante()
+

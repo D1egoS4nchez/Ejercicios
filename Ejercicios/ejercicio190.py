9ccadc5fdcfba576cd1eb76512683a792e3f6b2d
Diego <sanchezramrezdiego@gmail.com>
2021-01-05 07:57:43 -0600

    Ejercicio 190
    Falta comentar

--- Ejercicios/ejercicio190.py
+++ Ejercicios/ejercicio190.py
@@ -10,4 +10,23 @@ por último uno que imprima un mensaje si debe pagar impuestos (si el sueldo sup
 
 class Empleado:
     def __init__(self):
-        
+        self.nombre = input("Ingrese el nombre del empleados: ")
+        self.sueldo = int(input("Ingrese el sueldo de {}: ".format(self.nombre)))
+
+    def imprimir(self):
+        print("Nombre: {}".format(self.nombre))
+        print("Sueldo: {}".format(self.sueldo))
+
+
+    def paga_impuestos(self):
+        if self.sueldo > 3000:
+            print("{} debe de pagar impuestos".format(self.nombre))
+        else:
+            print("{} no debe de pagar impuestos".format(self.nombre))
+
+
+#main block
+
+empleado1 = Empleado()
+empleado1.imprimir()
+empleado1.paga_impuestos()

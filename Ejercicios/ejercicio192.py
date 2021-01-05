2f888a57187af429374db19622e7720869a27ea9
Diego <sanchezramrezdiego@gmail.com>
2021-01-05 08:26:09 -0600

    Ejercicio192, no esta terminado, solo con indicaciones

---
+++ Ejercicios/ejercicio192.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+"""
+Desarrollar una clase que represente un Cuadrado y tenga los siguientes métodos: inicializar el valor del lado 
+llegando como parámetro al método __init__ (definir un atributo llamado lado), imprimir su perímetro y su superficie.
+""

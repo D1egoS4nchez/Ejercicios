cc4347c67383da412e0914424e4fc90b9baf732b
Diego <sanchezramrezdiego@gmail.com>
2021-01-05 08:33:56 -0600

    Ejercicio193, se puso las indicaciones

---
+++ Ejercicios/ejercicio193.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+"""
+Implementar la clase Operaciones. Se deben cargar dos valores enteros por teclado en el método __init__, 
+calcular su suma, resta, multiplicación y división, cada una en un método, imprimir dichos resultados.
+"""

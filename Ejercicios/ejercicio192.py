9c8d059dcc5fb84017441b6eb3ef24e57c738e59
Diego <sanchezramrezdiego@gmail.com>
2021-01-05 08:32:38 -0600

    Ejercicio 192 terminado en su totalidad, solo hace falta comentar

--- Ejercicios/ejercicio192.py
+++ Ejercicios/ejercicio192.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
+import math
 
 """
 Desarrollar una clase que represente un Cuadrado y tenga los siguientes métodos: inicializar el valor del lado 
 llegando como parámetro al método __init__ (definir un atributo llamado lado), imprimir su perímetro y su superficie.
-""
+"""
+
+class Cuadrado:
+
+    def __init__(self,lado):
+        self.lado = lado
+
+
+    def imprimir(self):
+        print("Su superficie es {}".format(pow(self.lado,2)))
+        print("Su perimetro es {}".format(self.lado * 4))
+
+
+
+#MAIN BLOCK
+
+cuadrado = Cuadrado(6)
+cuadrado.imprimir()

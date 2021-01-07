8dcfbd26a947d0dfa9d1a61600a683f8abfa3b9f
Diego <sanchezramrezdiego@gmail.com>
2021-01-07 13:11:02 -0600

    Ejercicio198, no se ha comentado

--- Ejercicios/ejercicio198.py
+++ Ejercicios/ejercicio198.py
@@ -13,3 +13,35 @@ class Dado:
         self.valor = random.randint(1,6)
 
     def imprimir(self):
+        print("Valor del dado {}".format(self.valor))
+
+    def retonar_valor(self):
+
+        return self.valor
+
+
+class JuegoDeDados:
+
+
+    def __init__(self):
+        self.dado1 = Dado()
+        self.dado2 = Dado()
+        self.dado3 = Dado()
+
+    
+    def jugar(self):
+        self.dado1.tirar()
+        self.dado1.imprimir()
+        self.dado2.tirar()
+        self.dado2.imprimir()
+        self.dado3.tirar()
+        self.dado3.imprimir()
+        if self.dado1.retonar_valor() == self.dado2.retonar_valor() and self.dado1.retonar_valor()==self.dado3.retonar_valor():
+            print("Gano")
+        else:
+            print("Perdio")
+
+
+
+juego_dados = JuegoDeDados()
+juego_dados.jugar()

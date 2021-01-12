bdeae876b543c2aad9ae2aae2a98186b8fb96f9e
Diego <sanchezramrezdiego@gmail.com>
2021-01-12 17:43:17 -0600

    Herencia de una clase

--- Ejercicios/ejercicio200.py
+++ Ejercicios/ejercicio200.py
@@ -10,4 +10,44 @@ También en el bloque principal del programa crear un objeto de la clase Emplead
 """
 
 class Persona:
+    
 
+    def __init__(self):
+
+        self.nombre = input("Ingrese el nombre: ")
+        self.edad = int(input("Ingrese la edad: "))
+
+
+    def imprimir(self):
+        print("Nombre: ",self.nombre)
+        print("Edad: ",self.edad)
+
+
+class Empleado:
+
+    def __init__(self):
+        super().__init__()
+        self.sueldo = float(input("Ingrese el sueldo de {}: ".format(self.nombre)))
+
+
+    def imprimir(self):
+        super().imprimir()
+        print("Sueldo: ".format(self.sueldo))
+
+
+    def paga_impuestos(self):
+        if self.sueldo > 3000:
+            print("Debe de pagar impuestos {}".format(self.nombre))
+        else:
+            print("No debe de pagar impuestos {}".format(self.nombre))
+
+
+#MAIN BLOCK
+
+persona1 = Persona()
+persona1.imprimir()
+print("_____________________________")
+
+empleado1 = Empleado()
+empleado1.imprimir()
+empleado1.paga_impuestos()

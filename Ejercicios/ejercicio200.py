e6042f22128624ac57a2dffaefd8cf38b1531fcf
Diego <sanchezramrezdiego@gmail.com>
2021-01-12 18:18:12 -0600

    Herencia

--- Ejercicios/ejercicio200.py
+++ Ejercicios/ejercicio200.py
@@ -9,25 +9,35 @@ Declarar una segunda clase llamada Empleado que herede de la clase Persona y agr
 También en el bloque principal del programa crear un objeto de la clase Empleado.
 """
 
+#Se hace la clase padre que se llama Persona
+
 class Persona:
     
-
+    #Inicializamos los atributos que va a tener la clase padre, que son nombre y edad 
     def __init__(self):
 
         self.nombre = input("Ingrese el nombre: ")
         self.edad = int(input("Ingrese la edad: "))
-
+    
+    #Imprimimos los atributos de la clase, en este caso lo que ingresemos cuando se declare el objeto va a mostrarlo
 
     def imprimir(self):
         print("Nombre: ",self.nombre)
         print("Edad: ",self.edad)
 
 
-class Empleado:
 
+#Clase hija
+
+#La herencia se presenta en la clase Empleado, en la declaración de la clase indicamos entre paréntesis el nombre de la clase de la cual hereda
+class Empleado(Persona):
+    
+
+    #Metodo init
     def __init__(self):
+        #Con el metodo super, agarramos de la clase padre el metodo init de el
         super().__init__()
-        self.sueldo = float(input("Ingrese el sueldo de {}: ".format(self.nombre)))
+        self.sueldo = float(input("Ingrese el sueldo: "))
 
 
     def imprimir(self):
@@ -37,9 +47,9 @@ class Empleado:
 
     def paga_impuestos(self):
         if self.sueldo > 3000:
-            print("Debe de pagar impuestos {}".format(self.nombre))
+            print("Debe de pagar impuestos")
         else:
-            print("No debe de pagar impuestos {}".format(self.nombre))
+            print("No debe de pagar impuestos")
 
 
 #MAIN BLOCK
@@ -48,6 +58,6 @@ persona1 = Persona()
 persona1.imprimir()
 print("_____________________________")
 
-empleado1 = Empleado()
+empleado1=Empleado()
 empleado1.imprimir()
 empleado1.paga_impuestos()
